59a910b194ed430ecab9485087fad6e134d01f4f
machelinos <marcel.cabrera@gmail.com>
2023-06-04 21:17:31 -0500

    Initial commit

---
+++ src/GiffExpertApp.jsx
@@ -0,0 +1,5 @@
+export const GiffExpertApp = () => {
+  return (
+    <h1>GiffExpertApp</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GiffExpertApp } from './GiffExpertApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GiffExpertApp />
+  </React.StrictMode>,
+)
